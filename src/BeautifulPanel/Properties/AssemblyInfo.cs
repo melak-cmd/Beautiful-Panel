6ac99c7d0764c556c13748f8c5045a0b2e04490d
Amine <a18m02k86_pro@outlook.com>
2020-04-03 21:13:25 +0200

    init project struc

---
+++ src/BeautifulPanel/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BeautifulPanel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mohamed EL Amine KAOUANI")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright ©  Mohamed EL Amine KAOUANI 2009")]
+[assembly: AssemblyTrademark("Mohamed EL Amine KAOUANI")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("48691351-a3e4-4621-9a7a-fd6ec459206e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
