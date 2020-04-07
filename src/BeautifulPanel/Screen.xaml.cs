ac5865648285a64fe3b20c675a0262a4297301e5
Amine <a18m02k86_pro@outlook.com>
2020-04-07 19:57:25 +0200

    update code struc

--- src/BeautifulPanel/Screen.xaml.cs
+++ src/BeautifulPanel/Screen.xaml.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media.Animation;
 
 namespace BeautifulPanel
 {
     public partial class Screen
     {
-        private readonly Random _random = new Random();
         private int _directionX, _directionY;
         private double _screenHeight, _screenWidth;
 
@@ -28,22 +22,9 @@ namespace BeautifulPanel
 
         private void Populate()
         {
-            NewMethod(LayerA, 10,1.0, 0.9);
-            NewMethod(LayerB, 20,0.6, 0.5);
-            NewMethod(LayerC, 30, 0.3, .2);
-
-        }
-
-        private void GenerateShape(Panel panel, int nb,double scale, double opacity)
-        {
-            int count;
-            for (count = 0; count < nb; count++)
-            {
-                var flake = new Flake(_random.Next(0, 360), scale, opacity);
-                Canvas.SetLeft(flake, _random.Next(0, Convert.ToInt32(_screenWidth)));
-                Canvas.SetTop(flake, _random.Next(0, Convert.ToInt32(_screenHeight)));
-                panel.Children.Add(flake);
-            }
+            LayerA.GenerateShape(10, 1.0, 0.9, _screenWidth, _screenHeight);
+            LayerB.GenerateShape(20, 0.6, 0.5, _screenWidth, _screenHeight);
+            LayerC.GenerateShape(30, 0.3, 0.2, _screenWidth, _screenHeight);
         }
 
         private void LayoutRoot_MouseEnter(object sender, MouseEventArgs e)
@@ -59,67 +40,15 @@ namespace BeautifulPanel
             var positionX = position.X / _screenWidth;
             var positionY = position.Y / _screenHeight;
 
-            BuildAnimation("LayerControls", _directionX * 1920 * positionX, _directionX * 1080 * positionY, StoryboardLayerControl);
-            BuildAnimation("LayerA", _directionX * 1080.0 * positionX, _directionY * 900.0 * positionY, StoryboardLayerA);
-            BuildAnimation("LayerB", _directionX * 700.0 * positionX, _directionY * 450.0 * positionY, StoryboardLayerB);
-            BuildAnimation("LayerC", _directionX * 400.0 * positionX, _directionY * 300.0 * positionY, StoryboardLayerC);
+            StoryboardLayerControl.BuildAnimation("LayerControls", _directionX * 1920 * positionX, _directionX * 1080 * positionY);
+            StoryboardLayerA.BuildAnimation("LayerA", _directionX * 1080.0 * positionX, _directionY * 900.0 * positionY);
+            StoryboardLayerB.BuildAnimation("LayerB", _directionX * 700.0 * positionX, _directionY * 450.0 * positionY);
+            StoryboardLayerC.BuildAnimation("LayerC", _directionX * 400.0 * positionX, _directionY * 300.0 * positionY);
 
             StoryboardLayerA.Begin();
             StoryboardLayerB.Begin();
             StoryboardLayerC.Begin();
             StoryboardLayerControl.Begin();
-        }
-
-        private void BuildAnimation(string targetName, double x, double y, Storyboard storyboard)
-        {
-            DoubleAnimationUsingKeyFrames animation1, animation2;
-
-            if (storyboard.Children.Any())
-            {
-                animation1 = storyboard.Children[0] as DoubleAnimationUsingKeyFrames;
-                animation2 = storyboard.Children[1] as DoubleAnimationUsingKeyFrames;
-                animation1.KeyFrames[0].Value = x;
-                animation2.KeyFrames[0].Value = y;
-            }
-            else
-            {
-                animation1 = new DoubleAnimationUsingKeyFrames
-                {
-                    BeginTime = new TimeSpan(0, 0, 0)
-                };
-                animation2 = new DoubleAnimationUsingKeyFrames
-                {
-                    BeginTime = new TimeSpan(0, 0, 0)
-                };
-
-                Storyboard.SetTargetName(animation1, targetName);
-                Storyboard.SetTargetName(animation2, targetName);
-
-                Storyboard.SetTargetProperty(animation1,
-                    new PropertyPath("(UIElement.RenderTransform).(TransformGroup.Children)[3].(TranslateTransform.X)"));
-                Storyboard.SetTargetProperty(animation2,
-                    new PropertyPath("(UIElement.RenderTransform).(TransformGroup.Children)[3].(TranslateTransform.Y)"));
-
-                var translate1ControlX = new SplineDoubleKeyFrame
-                {
-                    Value = x,
-                    KeyTime = KeyTime.FromTimeSpan(new TimeSpan(0, 0, 0, 0, 500)),
-                    KeySpline = new KeySpline { ControlPoint1 = new Point(0, 0), ControlPoint2 = new Point(0.5, 1) }
-                };
-
-                var translate1ControlY = new SplineDoubleKeyFrame
-                {
-                    Value = y,
-                    KeyTime = KeyTime.FromTimeSpan(new TimeSpan(0, 0, 0, 0, 500)),
-                    KeySpline = new KeySpline { ControlPoint1 = new Point(0, 0), ControlPoint2 = new Point(0.5, 1) }
-                };
-
-                animation1.KeyFrames.Add(translate1ControlX);
-                animation2.KeyFrames.Add(translate1ControlY);
-
-                storyboard.Children.Add(animation1);
-                storyboard.Children.Add(animation2);
-            }
         }
 
         private void LayoutRoot_SizeChanged(object sender, SizeChangedEventArgs e)
---
+++ src/BeautifulPanel/Utils.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace BeautifulPanel
+{
+    public static class Utils
+    {
+        internal static void GenerateShape(this Panel panel, int nb, double scale, double opacity, double width, double height)
+        {
+            Random random = new Random();
+            int count;
+            for (count = 0; count < nb; count++)
+            {
+                ShapeBase shape;
+                shape = new Flake(random.Next(0, 360), scale, opacity);
+                Canvas.SetLeft(shape, random.Next(0, Convert.ToInt32(width)));
+                Canvas.SetTop(shape, random.Next(0, Convert.ToInt32(height)));
+                panel.Children.Add(shape);
+            }
+        }
+
+        internal static void BuildAnimation(this Storyboard storyboard, string targetName, double x, double y)
+        {
+            DoubleAnimationUsingKeyFrames animation1, animation2;
+
+            if (storyboard.Children.Any())
+            {
+                animation1 = storyboard.Children[0] as DoubleAnimationUsingKeyFrames;
+                animation2 = storyboard.Children[1] as DoubleAnimationUsingKeyFrames;
+                animation1.KeyFrames[0].Value = x;
+                animation2.KeyFrames[0].Value = y;
+            }
+            else
+            {
+                animation1 = new DoubleAnimationUsingKeyFrames
+                {
+                    BeginTime = new TimeSpan(0, 0, 0)
+                };
+                animation2 = new DoubleAnimationUsingKeyFrames
+                {
+                    BeginTime = new TimeSpan(0, 0, 0)
+                };
+
+                Storyboard.SetTargetName(animation1, targetName);
+                Storyboard.SetTargetName(animation2, targetName);
+
+                Storyboard.SetTargetProperty(animation1,
+                    new PropertyPath("(UIElement.RenderTransform).(TransformGroup.Children)[3].(TranslateTransform.X)"));
+                Storyboard.SetTargetProperty(animation2,
+                    new PropertyPath("(UIElement.RenderTransform).(TransformGroup.Children)[3].(TranslateTransform.Y)"));
+
+                var translate1ControlX = new SplineDoubleKeyFrame
+                {
+                    Value = x,
+                    KeyTime = KeyTime.FromTimeSpan(new TimeSpan(0, 0, 0, 0, 500)),
+                    KeySpline = new KeySpline { ControlPoint1 = new Point(0, 0), ControlPoint2 = new Point(0.5, 1) }
+                };
+
+                var translate1ControlY = new SplineDoubleKeyFrame
+                {
+                    Value = y,
+                    KeyTime = KeyTime.FromTimeSpan(new TimeSpan(0, 0, 0, 0, 500)),
+                    KeySpline = new KeySpline { ControlPoint1 = new Point(0, 0), ControlPoint2 = new Point(0.5, 1) }
+                };
+
+                animation1.KeyFrames.Add(translate1ControlX);
+                animation2.KeyFrames.Add(translate1ControlY);
+
+                storyboard.Children.Add(animation1);
+                storyboard.Children.Add(animation2);
+            }
+        }
+    }
+}
